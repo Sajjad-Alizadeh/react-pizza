33cf31aa9c0393f8a6e1db4a75ce1880c8ae9387
Sajjad Alizadeh <sajjadalizzh@gmail.com>
2023-08-29 12:30:14 +0330

    add redux & impl user reducer

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
 import {Form, redirect, useActionData, useNavigation} from "react-router-dom";
 import {createOrder} from "../../services/apiRestaurant.js";
+import {useSelector} from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -37,6 +38,7 @@ function CreateOrder() {
   const actionData = useActionData()
   const navigation = useNavigation()
   const isSubmitting = navigation.state === "submitting"
+  const username = useSelector(state => state.user.username)
 
   return (
     <div className="px-4 py-6">
@@ -45,7 +47,11 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input className="input grow"
+                 type="text"
+                 name="customer"
+                 defaultValue={username}
+                 required />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,7 +1,11 @@
 import CreateUser from '../features/user/CreateUser';
+import { useSelector } from "react-redux";
+import Button from "./Button.jsx";
 
 function Home() {
-  return (
+    const username = useSelector(state => state.user.username)
+
+    return (
     <div className="my-10 px-4 text-center sm:my-16">
       <h1 className="mb-8  text-xl font-semibold md:text-3xl">
         The best pizza.
@@ -10,8 +14,14 @@ function Home() {
           Straight out of the oven, straight to you.
         </span>
       </h1>
-
-      <CreateUser />
+        {
+            username ?
+                <Button to="/menu" type="primary">
+                    Continue ordering, {username}
+                </Button>
+                :
+                <CreateUser />
+        }
     </div>
   );
 }
